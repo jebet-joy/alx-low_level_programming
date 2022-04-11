eb2a12af5414d885017ebe0181933c5a806372e8
jebet-joy <jebetjoey@gmail.com>
2022-04-11 18:27:13 +0300

    Task 8, Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Funtion begins
- * Return: Finction ends
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful
+	write(STDOUT_FILENO, "and that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
