d8321070712b630817e5d1f9449d2285214f3082
jebet-joy <jebetjoey@gmail.com>
2022-04-15 01:40:15 +0300

    A function that checks for uppercase character

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,12 @@
-#include "main.h"
-
-/**
- * _isupper - checks for uppercase character
- * @letter: the character to be tracked
- * Return: 1 if c is uppercase, 0 otherwise
- */
-int _isupper(int letter)
-{
-	return (letter >= 'A' && letter <= 'Z');
-}
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);i
