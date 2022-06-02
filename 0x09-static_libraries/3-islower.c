da3d7eb3dfa600e010d11a903bbd961da943f469
jebet-joy <jebetjoey@gmail.com>
2022-06-02 14:32:41 +0300

    Static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - checks lowercase character
+ *@c: represents value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
